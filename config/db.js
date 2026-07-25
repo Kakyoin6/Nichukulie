0bc21e578b8a6710fbc547310ab6fd054117e36d
Nimrod <138994111+Kakyoin6@users.noreply.github.com>
2026-07-25 23:18:36 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,1 +1,14 @@
 
+const mongoose = require('mongoose');
+
+function connectMongo() {
+  if (!process.env.MONGO_URI) {
+    console.warn('MongoDB not connected: set MONGO_URI in .env to enable database features.');
+    return;
+  }
+  mongoose.connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 3000 })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB error:', err.message));
+}
+
+module.exports = connectMongo;
